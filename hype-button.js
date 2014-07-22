be822a9499a9cce149dab2db9538a183181e2a11
bdubaya <waters.bennie@gmail.com>
2014-07-22 12:42:13 -0400

    what have i done.

--- hype-button.js
+++ hype-button.js
@@ -1,3 +1,4 @@
+@@ -1,75 +0,0 @@
 //Custom javascript for Bennie.com
 
 //hype-button
@@ -13,9 +14,65 @@
 				{
 					duration: 2000,
 					step: function (now) {
-						b.css('background', 'radial-gradient(circle 80px at ' + now + 'px 30px, #ffaaaa, red)')
+						b.css('background', 'radial-gradient(circle 80px at ' + now + 'px 30px, #F4FFAA, yellow)')
 					}
 				})
-		}, 8000);
+		}, 6000);
+		
+		b.hover(function () {
+            $({
+                sc: '1'
+            }).animate({
+                sc: '1.1'
+            }, {
+                duration: 1000,
+                easing: 'easeOutElastic',
+                step: function (now, tween) {
+                    b.css('transform', 'scale(' + now + ')');
+                    b.children().css('transform', 'scale(' + now * 1.1 + ')');
+                }
+            });
+        },
+			
+		function () {
+			$({
+				sc: '1.1'
+			}).animate({
+				sc: '1'
+			}, {
+				duration: 1000,
+				easing: 'easeOutElastic',
+				step: function (now, tween) {
+					b.css('transform', 'scale(' + now + ')');
+					b.children().css('transform', 'scale(' + now + ')');
+				}
+			});
+			
+		});
+
+		b.click(function (){
+			b.unbind('mouseenter mouseleave click');
+			
+			//fade the text
+			b.find('#hype').fadeOut(500);
+			
+			//move the paw
+			var middleSlider = b.width() * .5;
+			b.find('#hype-icon').animate({ left: b.width() - 20 }, 2000, 'easeOutBounce');
+			//b.find('i').animate({ left: '500' }, 2000, 'easeOutBounce');
+			
+			//tilt the entire button
+			$({tilt: 0}).animate({tilt: 20},
+				{
+					duration: 1000,
+					step: function (tilt) {
+						b.css('transform', 'rotate(' + tilt + 'deg)');
+					},
+					easing: 'easeOutBounce'
+				});
+		});
+		
 	}
 }(window.PS = window.PS || {}));
+ No newline at end of file
+
