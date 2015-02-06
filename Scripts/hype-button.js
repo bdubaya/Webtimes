ddcbe91008674cb4305a0dfbfe053d72d47c05d0
bdubaya <waters.bennie@gmail.com>
2015-02-06 10:59:35 -0500

    fixed everything

--- Scripts/hype-button.js
+++ Scripts/hype-button.js
@@ -4,21 +4,25 @@
 
 (function (ns) {
 
-	ns.HypeButton = function (id) {
+    ns.HypeButton = function (id, cb) {
+
+        var b = $("#" + id);
+		var callback = cb;
 		
-		var b = $("#" + id);
-		
-		setInterval(function () {
-			$({ xval: -30 }).animate({ xval: 260},
-				{
-					duration: 2000,
-					step: function (now) {
-						b.css('background', 'radial-gradient(circle 80px at ' + now + 'px 30px, #F4FFAA, yellow)')
-					}
-				})
-		}, 6000);
-		
-		b.hover(function () {
+		b.fadeIn('slow', function(){
+			setInterval(function () {
+				$({ xval: -30 }).animate({ xval: 300 },
+					{
+						duration: 2000,
+						step: function (now) {
+							b.css('background', 'radial-gradient(circle 80px at ' + now + 'px 30px, #F4FFAA, yellow)');
+						}
+					})
+			}, 4000);
+		});
+        
+
+        b.hover(function () {
             $({
                 sc: '1'
             }).animate({
@@ -32,44 +36,56 @@
                 }
             });
         },
-			
-		function () {
-			$({
-				sc: '1.1'
-			}).animate({
-				sc: '1'
-			}, {
-				duration: 1000,
-				easing: 'easeOutElastic',
-				step: function (now, tween) {
-					b.css('transform', 'scale(' + now + ')');
-					b.children().css('transform', 'scale(' + now + ')');
-				}
-			});
-			
-		});
 
-		b.click(function (){
+            function () {
+                $({
+                    sc: '1.1'
+                }).animate({
+                    sc: '1'
+                }, {
+                    duration: 1000,
+                    easing: 'easeOutElastic',
+                    step: function (now, tween) {
+                        b.css('transform', 'scale(' + now + ')');
+                        b.children().css('transform', 'scale(' + now + ')');
+                    }
+                });
+            });
+		
+		b.click(function() {
+			
 			b.unbind('mouseenter mouseleave click');
 			
 			//fade the text
-			b.find('#hype').fadeOut(500);
-			
+			b.find('span').fadeOut(500);
+			b.hide("explode", {pieces: 30}, 2000);
+			/*
 			//move the paw
-			var middleSlider = b.width() * .5;
-			b.find('#hype-icon').animate({ left: b.width() - 20 }, 2000, 'easeOutBounce');
-			//b.find('i').animate({ left: '500' }, 2000, 'easeOutBounce');
+			var ico = b.find('i');
+			ico.animate({ left: '90%' }, 1000);
+			ico.animate({ left: '0%' }, 1000);
+			ico.animate({ left: '50%' }, 1000);
 			
-			//tilt the entire button
-			$({tilt: 0}).animate({tilt: 20},
+			
+			//tilt the button
+			$({ tilt: 0 }).animate({ tilt: 360 },
 				{
-					duration: 1000,
+					duration: 3000,
 					step: function (tilt) {
 						b.css('transform', 'rotate(' + tilt + 'deg)');
+						//ico.css('transform', 'rotate(' - tilt + 'deg)');
 					},
-					easing: 'easeOutBounce'
+					complete: function() {
+						b.fadeOut(1000, function () {
+							callback();
+						});
+					}
 				});
+				*/
 		});
 		
-	}
+
+    }
 }(window.PS = window.PS || {}));
+
+
--- readme.txt
+++
@@ -1,3 +0,0 @@
-Requires any text editor and browser.
-Visual Studio Express 2012 is used for demos, but any HTML editor is fine.
-Internet access to acquire jQuery and other libraries required.
